ebaf159bbbeee2f3f9fc75414d2259cf5b50b172
Hayden Woods <hadywady@gmail.com>
2019-08-18 18:34:48 +0800

    Snap to grid + settings

--- SNAKPAK/SNAKPAK/MainWindow.xaml.cs
+++ SNAKPAK/SNAKPAK/MainWindow.xaml.cs
@@ -10,36 +10,88 @@ using System.Windows.Input;
 using System.Windows.Media;
 
 namespace SNAKPAK {
+    public static class ExtensionMethods {
+        public static int RoundOff(int num, int snap) {
+            return (int)((int)Math.Round(num / (double)snap) * snap);
+        }
+    }
+
     public partial class MainWindow : Window {
-        void createComputers(int count) {
+        //Settings Controls
+        readonly RoutedEventHandler ToggleSnapToGrid = (sender, args) => {
+            if (Properties.Settings.Default.SnapToGrid == true) {
+                Properties.Settings.Default.SnapToGrid = false;
+            } else {
+                Properties.Settings.Default.SnapToGrid = true;
+            }
+        };
+        readonly TextChangedEventHandler ChangeSnapAmount = (sender, args) => {
+            var element = (TextBox)sender;
+            if (int.TryParse(element.Text, out int n)) {
+                Properties.Settings.Default.SnapAmount = n;
+            }
+        };
+
+        //Settings Setup
+        void SettingsBindings() {
+            SnapToGrid.Checked += ToggleSnapToGrid;
+            SnapToGrid.Unchecked += ToggleSnapToGrid;
+            SnapAmount.TextChanged += ChangeSnapAmount;
+        }
+        void SettingsDefaults() {
+            SnapToGrid.IsChecked = Properties.Settings.Default.SnapToGrid;
+            SnapAmount.Text = Properties.Settings.Default.SnapAmount.ToString();
+
+            SettingsBindings();
+        }
+
+        void CreateComputers(int count) {
             Nullable<Point> dragStart = null;
 
+            //Left mouse down
             MouseButtonEventHandler mouseDown = (mouseSender, args) => {
-                var element = (UIElement)mouseSender;
+                var element = (FrameworkElement)mouseSender;
                 dragStart = args.GetPosition(element);
                 element.CaptureMouse();
             };
+            //Left mouse up
             MouseButtonEventHandler mouseUp = (mouseSender, args) => {
-                var element = (UIElement)mouseSender;
+                var element = (FrameworkElement)mouseSender;
                 dragStart = null;
                 element.ReleaseMouseCapture();
             };
+            //On mouse move
             MouseEventHandler mouseMove = (mouseSender, args) => {
-                if (dragStart != null && args.LeftButton == MouseButtonState.Pressed)
-                {
-                    var element = (UIElement)mouseSender;
+                if (dragStart != null && args.LeftButton == MouseButtonState.Pressed) {
+                    var element = (FrameworkElement)mouseSender;
                     var p2 = args.GetPosition(computerCanvas);
-                    
-                    if ((p2.X - dragStart.Value.X) > 0 && (p2.X + dragStart.Value.X) < computerCanvas.ActualWidth)
-                    {
-                        Canvas.SetLeft(element, p2.X - dragStart.Value.X);
+
+                    if ((p2.X - dragStart.Value.X) > 0 && (p2.X - dragStart.Value.X + element.ActualWidth) < computerCanvas.ActualWidth) {
+                        double newX = p2.X - dragStart.Value.X;
+                        if (Properties.Settings.Default.SnapToGrid == true) {
+                            newX = ExtensionMethods.RoundOff((int)newX, Properties.Settings.Default.SnapAmount);
+
+                            if ((newX + element.ActualWidth) > computerCanvas.ActualWidth) {
+                                newX = newX - Properties.Settings.Default.SnapAmount;
+                            }
+                        }
+                        Canvas.SetLeft(element, newX);
                     }
-                    if ((p2.Y - dragStart.Value.Y) > 0 && (p2.Y + dragStart.Value.Y) < computerCanvas.ActualHeight)
-                    {
-                        Canvas.SetTop(element, p2.Y - dragStart.Value.Y);
+                    if ((p2.Y - dragStart.Value.Y) > 0 && (p2.Y - dragStart.Value.Y + element.ActualHeight) < computerCanvas.ActualHeight) {
+                        double newY = p2.Y - dragStart.Value.Y;
+                        if (Properties.Settings.Default.SnapToGrid == true) {
+                            newY = ExtensionMethods.RoundOff((int)newY, Properties.Settings.Default.SnapAmount);
+
+                            if ((newY + element.ActualHeight) > computerCanvas.ActualHeight) {
+                                newY = newY - Properties.Settings.Default.SnapAmount;
+                            }
+                        }
+
+                        Canvas.SetTop(element, newY);
                     }
                 }
             };
+            //Sets the bindings for the mouse movements
             Action<UIElement> enableDrag = (element) => {
                 element.PreviewMouseDown += mouseDown;
                 element.PreviewMouseMove += mouseMove;
@@ -47,26 +99,33 @@ namespace SNAKPAK {
             };
 
             //Creates the computer boxes on the screen
-            /*
-            <Buttons>
-                <Rectangle>
-
-                </Rectangle>
-            </Button>
-            */
-            for (int i = 0; i < count; i++) {
+            for (int i = 1; i < count + 1; i++) {
                 Button newComputer = new Button();
                 newComputer.Name = "Computer" + i.ToString();
-                newComputer.BorderThickness = new Thickness(0);
-                Canvas.SetTop(newComputer, 0);
-                Canvas.SetLeft(newComputer, (140 * i));
+                //Apply style to remove button outline
+                newComputer.Style = Resources["TransparentStyle"] as Style;
+
+                Grid grid = new Grid();
 
                 Rectangle display = new Rectangle();
-                display.Width = 120;
+                display.Width = 100;
                 display.Height = 50;
-                display.Fill = Brushes.Black;
+                display.Fill = Brushes.Beige;
+                display.Stroke = Brushes.Black;
+                display.StrokeThickness = 1;
 
-                newComputer.Content = display;
+                Border border = new Border();
+                TextBlock text = new TextBlock();
+                text.Text = newComputer.Name;
+                text.FontSize = 16;
+                text.TextAlignment = TextAlignment.Center;
+                text.Foreground = Brushes.Black;
+                text.VerticalAlignment = VerticalAlignment.Center;
+                border.Child = text;
+
+                grid.Children.Add(display);
+                grid.Children.Add(border);
+                newComputer.Content = grid;
 
                 enableDrag(newComputer);
                 computerCanvas.Children.Add(newComputer);
@@ -74,7 +133,8 @@ namespace SNAKPAK {
         }
 
         void OnPageLoad(object sender, RoutedEventArgs e) {
-            createComputers(20);
+            SettingsDefaults();
+            CreateComputers(20);
         }
     }
 }
--- SNAKPAK/SNAKPAK/Properties/Settings.Designer.cs
+++ SNAKPAK/SNAKPAK/Properties/Settings.Designer.cs
@@ -8,22 +8,42 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SNAKPAK.Properties
-{
-
-
+namespace SNAKPAK.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.2.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool SnapToGrid {
+            get {
+                return ((bool)(this["SnapToGrid"]));
+            }
+            set {
+                this["SnapToGrid"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("10")]
+        public int SnapAmount {
+            get {
+                return ((int)(this["SnapAmount"]));
+            }
+            set {
+                this["SnapAmount"] = value;
             }
         }
     }
